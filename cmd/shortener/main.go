bb2a63b73365a8a58dcc61cd866f9693fee28712
lekan <lekan.lekan@gmail.com>
2021-11-18 10:55:55 +0300

    server

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+)
+
+func CreateShortURLHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+}
+
+func GetShortURLById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+
+}
+
+func main() {
+	router := httprouter.New()
+	router.POST("/", CreateShortURLHandler)
+	router.GET("/:id", GetShortURLById)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lekan-pvp/incr1
 
 go 1.17
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
