3eca0f4f70c3f5f0ccd20806f91cfa19f8a1d2a1
lekan <lekan.lekan@gmail.com>
2021-11-18 12:30:39 +0300

    maybe done

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"github.com/julienschmidt/httprouter"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 )
 
-func CreateShortURLHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+const prefix = "localhost:8080/"
 
+type URLs struct {
+	Long string
+	Short string
+}
+
+var shorts map[int]URLs
+
+var ID = 1
+
+func CreateShortURLHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	id := &ID
+	long, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.WriteHeader(201)
+	short := fmt.Sprintf("%s%d", prefix, id)
+	shorts[*id] = URLs{
+		Long: string(long),
+		Short: short,
+	}
+	*id++
+	_, err = w.Write([]byte(short))
 }
 
 func GetShortURLById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
-
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+	w.WriteHeader(307)
+	w.Header().Set("Location", shorts[id].Long)
+	_, err = w.Write([]byte(""))
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+	}
 }
 
 func main() {
---
+++ internal/app/shorter/shorter.go
@@ -0,0 +1,13 @@
+package shorter
+
+import "math/rand"
+
+const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func Shorting() string {
+	b := make([]byte, 5)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
