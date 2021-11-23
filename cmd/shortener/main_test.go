e4eae1cb8e039abbdd9bbeff2800950115d600ce
lekan <lekan@list.ru>
2021-11-23 19:49:52 +0300

    fixed

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/lekan-pvp/incr1/internal/app/shorter"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
@@ -10,8 +11,66 @@ import (
 
 func TestGetURLByID(t *testing.T) {
 	type want struct {
+		contentType string
 		statusCode int
+	}
+	tests := []struct {
+		name string
+		request string
+		id int
+		long string
+		shorts map[int]URLs
+		want want
+	}{
+		{
+			name: "success",
+			request: "/1",
+			id: 1,
+			long: "http://google.com",
+			want: want{
+				contentType: "text/plain",
+				statusCode: 307,
+			},
+		},
+		{
+			name: "not found",
+			request: "/2",
+			long: "http://yandex.ru",
+			want: want{
+				contentType: "text/plain",
+				statusCode: 400,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for _, tt := range tests {
+				short := shorter.Shorting(1)
+				shorts[tt.id] = URLs{
+					Long: tt.long,
+					Short: short,
+				}
 
+				router := httprouter.New()
+				router.GET("/:id", GetURLByID)
+
+				req := httptest.NewRequest(http.MethodGet, tt.request, nil)
+
+				rr := httptest.NewRecorder()
+
+				router.ServeHTTP(rr, req)
+
+				result := rr.Result()
+				defer result.Body.Close()
+				assert.Equal(t, tt.want.statusCode, result.StatusCode)
+			}
+		})
+	}
+}
+
+func TestCreateShortURLHandler(t *testing.T) {
+	type want struct {
+		statusCode int
 	}
 	tests := []struct {
 		name string
@@ -20,26 +79,35 @@ func TestGetURLByID(t *testing.T) {
 	}{
 		{
 			name: "success",
-			request: "/1",
+			request: "/",
 			want: want{
-				statusCode: 307,
+				statusCode: 201,
+			},
+		},
+		{
+			name: "wrong endpoint",
+			request: "/wrong",
+			want: want{
+				statusCode: 404,
 			},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			router := httprouter.New()
-			router.GET("/:id", GetURLByID)
+			router.POST("/", CreateShortURLHandler)
 
-			req, err := http.NewRequest(http.MethodGet, tt.request, nil)
-			assert.NoError(t, err)
+			req := httptest.NewRequest(http.MethodPost, tt.request, nil)
+
 			rr := httptest.NewRecorder()
 
 			router.ServeHTTP(rr, req)
 
 			result := rr.Result()
+			defer result.Body.Close()
 
 			assert.Equal(t, tt.want.statusCode, result.StatusCode)
+
 		})
 	}
 }
